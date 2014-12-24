62508640a57f78e207f6849919b8e2542cfdffaf
Krishan Wyse <kwysek@gmail.com>
2014-12-24 15:51:51 +0000

    Remove references to fyp namespace

--- test/fyp/WorldTest.cpp
+++ test/fyp/WorldTest.cpp
@@ -1,4 +1,4 @@
-#include "fyp/World.hpp"
+#include "sgr/World.hpp"
 
 #include <Box2D/Box2D.h>
 #include <catch/catch.hpp>
@@ -10,6 +10,6 @@ TEST_CASE("World is initialized", "world")
   sf::RenderWindow window(sf::VideoMode(640, 480), "Test");
   sf::Vector2f gravity(0.f, -9.8f);
 
-  fyp::World world(window, gravity);
+  sgr::World world(window, gravity);
   REQUIRE(world.getPixelsPerMeter() == 16);
 }
