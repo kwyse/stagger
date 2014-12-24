bd52b12bac426cd38320b20457ab6f8e1e84e869
Krishan Wyse <kwysek@gmail.com>
2014-12-24 14:42:27 +0000

    Add CircleEntity

---
+++ include/sgr/CircleEntity.hpp
@@ -0,0 +1,45 @@
+// sgr/CircleEntity.hpp
+#ifndef SGR_CIRCLE_ENTITY_HPP_
+#define SGR_CIRCLE_ENTITY_HPP_
+
+
+#include <memory>
+#include "sgr/RenderEntity.hpp"
+
+
+namespace sgr
+{
+
+
+class CircleEntity : public RenderEntity
+{
+public:
+  CircleEntity(World* world,
+               float radius,
+               BodyType type = BodyType::STATIC,
+               unsigned int pointCount = 30);
+  CircleEntity(World* world,
+               float radius,
+               unsigned int pointCount,
+               BodyType type = BodyType::STATIC);
+
+  void update();
+
+  void setRadius(float radius);
+  void setPosition(float x, float y);
+  void setPosition(const sf::Vector2f& position);
+
+  float getRadius() const;
+  unsigned int getPointCount() const;
+  sf::Vector2f getPoint(unsigned int index) const;
+
+private:
+  float mRadius;
+  unsigned int mPointCount;
+};
+
+
+} // namespace sgr
+
+
+#endif // SGR_CIRCLE_ENTITY_HPP_
